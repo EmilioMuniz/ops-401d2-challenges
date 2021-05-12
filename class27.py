33116f78dee8d6e824cc89fbedeb22278171d16a
Emilio Muniz <emiliomunizphoto@gmail.com>
2021-05-11 22:33:19 -0700

    Create class27.py

---
+++ class27.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Script:                   ops-401d2-challenge-class26.py                       
+# Author:                   Emilio Muniz                       
+# Date of latest revision:  5/10/2021    
+# Purpose:                  Create logging capabilities.
+
+# Import Libraries
+import logging
+import os
+
+
+# Define variables
+logger = logging.getLogger('my_logger')
+handler = RotatingFileHandler('my_log.log', maxBytes=30, backupCount=1)
+logger.addHandler(handler)
+
+
+# Main
+logging.basicConfig(filename="./sample.log", level=logging.ERROR, format="%(asctime)s:%(levelname)s:%(message)s")
+print("Begin logging...")
+logging.debug("Debug this...")
+logging.info("Some info for you.")
+logging.warning("This is your warning.")
+
+try:
+  verification() # Intentional error
+except Exception as msg:
+  print(msg)
+  logging.exception(msg)
+print("Logging finished.")
+
+for i in range(10):
+    logmsg = "Hello world!"
+    logmsg += str(i)
+    logger.warning(logmsg)
+    print ("Logging Hello world! number", i)
+    os.system("ls -al")
+    time.sleep(1)

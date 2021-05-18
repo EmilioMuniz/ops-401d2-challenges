0b150a4be371f6a243665d1737505c10db380c25
Emilio Muniz <emiliomunizphoto@gmail.com>
2021-05-17 22:14:25 -0700

    Create class31.py

---
+++ class31.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Script:                   ops-401d2-challenge-class31.py                       
+# Author:                   Emilio Muniz                       
+# Date of latest revision:  5/17/2021    
+# Purpose:                  Signature-based Malware Detection Part 1 of 3.
+
+# Import Libraries
+import os
+
+# Define variables
+filename = input("Enter filename:")
+dirname = input("Enter directory name:")
+
+# Define functions
+
+# Main
+find(filename)
+

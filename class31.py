479c731d25602c771f8ec72aa43d1b9ba0a0ca17
Emilio Muniz <emiliomunizphoto@gmail.com>
2021-05-18 21:02:17 -0700

    Update class31.py

--- class31.py
+++ class31.py
@@ -6,14 +6,24 @@
 # Purpose:                  Signature-based Malware Detection Part 1 of 3.
 
 # Import Libraries
-import os
+import os, time
+from sys import platform
 
 # Define variables
-filename = input("Enter filename:")
-dirname = input("Enter directory name:")
 
 # Define functions
+def linuxsearch():
+    thefile = input("Enter the file you are searching for:")
+    thedirectory = input("Enter the directory to search:")
+    os.system("ls " + str(thedirectory) + " | echo \"Searched $(wc -l) files.\"")
+    os.system("find " + str(thedirectory) + ' -name ' + str(thefile) + " -print | echo \"Found $(grep -c /) files that mathced:\"")
+    print("")
+    os.system("find " + str(thedirectory) + ' -name ' + str(thefile))
+    print("")
 
 # Main
-find(filename)
+if platform == "linux" or platform == "linux2":
+    print("Searching Linux Machine.")
+    linuxsearch()
 
+# End for now.
